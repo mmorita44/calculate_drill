8856ef08fcda50aa13855c097383c6cde9ecbc13
Masato Morita <m.morita44@hotmail.com>
2020-03-22 15:23:06 +0900

    例外処理の追加

--- calculate_drill/calculate_drill/drill.py
+++ calculate_drill/calculate_drill/drill.py
@@ -1,36 +1,42 @@
 import random
 import openpyxl
+import traceback
 
 class Drill:
 
     def __init__(self):
         self.row_max = 27
         self.cloumn_max = 36
-        self.file = 'documents/足し算.xlsx'
+        self.file = 'document/足し算.xlsx'
 
     def calculate(self):
-        book = openpyxl.load_workbook(self.file)
-        sheet = book['Sheet1']
-        records = []
-        for first in range(1, 10):
-            for second in range(1, 10):
-                for third in range(1, 10):
-                    records.append([first, second, third])
-        random.shuffle(records)
+        try:
+            book = openpyxl.load_workbook(self.file)
+            sheet = book['Sheet1']
+            records = []
+            for first in range(1, 10):
+                for second in range(1, 10):
+                    for third in range(1, 10):
+                        records.append([first, second, third])
+            random.shuffle(records)
 
-        row_pos = 1
-        column_pos = 1
-        for record in records:
-            if row_pos >= self.row_max:
-                row_pos = 1
-                column_pos += 7
+            row_pos = 1
+            column_pos = 1
+            for record in records:
+                if row_pos >= self.row_max:
+                    row_pos = 1
+                    column_pos += 7
 
-            if column_pos >= self.cloumn_max:
-                break
+                if column_pos >= self.cloumn_max:
+                    break
 
-            sheet.cell(row=row_pos, column=column_pos, value=record[0])
-            sheet.cell(row=row_pos, column=column_pos + 2, value=record[1])
-            sheet.cell(row=row_pos, column=column_pos + 4, value=record[2])
-            row_pos += 2
+                sheet.cell(row=row_pos, column=column_pos, value=record[0])
+                sheet.cell(row=row_pos, column=column_pos + 2, value=record[1])
+                sheet.cell(row=row_pos, column=column_pos + 4, value=record[2])
+                row_pos += 2
 
-        book.save(self.file)
+            book.save(self.file)
+            return 0
+        except Exception as ignored:
+            print(traceback.format_exc())
+            return 1
